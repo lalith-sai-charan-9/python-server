58323568e3317fded12a00e3f14502ef44019115
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-01-14 16:49:20 +0530

    fixed the python server deployment issue

--- celery_worker.py
+++ celery_worker.py
@@ -9,8 +9,13 @@ REDIS_URL = os.getenv('REDIS_PUBLIC_URL', 'redis://localhost:6379/0')
 celery.conf.broker_url = REDIS_URL
 celery.conf.result_backend = REDIS_URL
 
-# Initialize the model within the worker
+# Initialize the model *within* the worker
 from app import download_model, get_model
 
-download_model()
-get_model()
+if __name__ == '__main__':
+    download_model()  # Download the model when the worker starts
+    get_model()      # Load the model when the worker starts
+    
+    # Now start the Celery worker
+    celery.worker_main() # Start celery worker
+
