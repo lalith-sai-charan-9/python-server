53c3fd6583996c08ef9fb06fcc40171fe989d53e
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-01-14 16:18:51 +0530

    resource optimization

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn app:app --bind 0.0.0.0:$PORT
-worker: celery -A celery_worker.celery worker --pool=solo -l debug --concurrency=1
+web: gunicorn app:app --bind 0.0.0.0:$PORT --timeout 300 --workers 1 --threads 1
+    worker: celery -A celery_worker.celery worker --pool=solo -l info --concurrency=1 --without-heartbeat --without-mingle
--- celery_worker.py
+++ celery_worker.py
@@ -1,10 +1,16 @@
 import os
-from app import celery, app
-
-# Push the Flask application context
-app.app_context().push()
-
-# Ensure Celery worker uses the same broker URL
-REDIS_URL = os.getenv('REDIS_PUBLIC_URL', 'redis://localhost:6379/0')
-celery.conf.broker_url = REDIS_URL
-celery.conf.result_backend = REDIS_URL
+    from app import celery, app
+    
+    # Push the Flask application context
+    app.app_context().push()
+    
+    # Ensure Celery worker uses the same broker URL
+    REDIS_URL = os.getenv('REDIS_PUBLIC_URL', 'redis://localhost:6379/0')
+    celery.conf.broker_url = REDIS_URL
+    celery.conf.result_backend = REDIS_URL
+    
+    # Initialize the model within the worker
+    from app import download_model, get_model
+    
+    download_model()
+    get_model()
